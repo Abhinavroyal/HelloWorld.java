4605bcf1e4e1ab737a4872a9c36f456d45415672
Abhinavroyal <87404665+Abhinavroyal@users.noreply.github.com>
2021-12-23 18:40:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     System.out.println("Hello world!");
     System.out.println("Happy new Year Abhinav");
     System.out.println("Happy Birthday Abhinav");
+    System.out.println("Happy New Year Abhinav")
   }
 }
